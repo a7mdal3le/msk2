8a6cdcdff0e3b11d3a91a565d01714256fa8e726
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-21 17:37:18 +0300

    Create 7ob00.js

---
+++ 7ob00.js
@@ -0,0 +1,23 @@
+var _0x27ec=[".nav-tabs","insertBefore","<form name="jump">
+<div class="des">
+					<map id="ColorsMap" name="ColorsMap">
+					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1">
+					<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle('Co2',60)" shape="rect" title="2">
+					<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle('Co3',60)" shape="rect" title="3">
+					<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle('Co4',60)" shape="rect" title="4">
+					<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle('Co5',60)" shape="rect" title="5">
+					</map>
+			        <img alt="" height="25" src="https://mskchat.com/sico/z1e8ra970c10.gif" width="300" usemap="#ColorsMap"></div>
+</form>
+
+","https://cdn.statically.io/gh/a7mdal3le/msk2/fa14f6bb/7ob1.js","getScript","width","100%","css","fl","addClass","body","<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<link href="https://cdn.statically.io/gh/a7mdal3le/msk2/fa14f6bb/7ob1.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.statically.io/gh/a7mdal3le/msk2/fa14f6bb/7ob2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />
+<link href="https://cdn.statically.io/gh/a7mdal3le/msk2/fa14f6bb/7ob3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />
+<link href="https://cdn.statically.io/gh/a7mdal3le/msk2/fa14f6bb/7ob4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />
+<link href="https://cdn.statically.io/gh/a7mdal3le/msk2/fa14f6bb/7ob5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />
+<style>
+
+</style>
+"];$(`${_0x27ec[2]}`)[_0x27ec[1]](_0x27ec[0]);$[_0x27ec[4]](_0x27ec[3]);$(_0x27ec[0])[_0x27ec[9]](_0x27ec[8])[_0x27ec[7]](_0x27ec[5],_0x27ec[6]);$(_0x27ec[0])[_0x27ec[9]](_0x27ec[8])[_0x27ec[7]](_0x27ec[5],_0x27ec[6]);$(`${_0x27ec[11]}`)[_0x27ec[1]](_0x27ec[10])
